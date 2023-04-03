86ff36baa5d8ec9538d9138b95a41a7095e0f3dd
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-04-03 03:35:54 +0000

    ex11 done

--- ex11/find.py
+++ ex11/find.py
@@ -1,9 +1,22 @@
 import requests
+import json
 
-isname = True
-while isname:
-	pokiname = input('Enter the name of a Pokemon: ')
-	if not pokiname or pokiname == '':
-		print('Please enter the name of a Pokemon.')
-	isname = False
+while True:
+    try:
+        pokiname = input('Enter the name of a Pokemon: ')
+    except EOFError:
+        continue
+    if not pokiname or pokiname == '':
+        print('Please enter the name of a Pokemon.')
+        continue
+    break
 api_url = f'https://pokeapi.co/api/v2/pokemon/{pokiname}'
+response = requests.get(api_url)
+if response.status_code == 200:
+    rspn = json.loads(response.content)
+    print(f'Name: ' + rspn['name'])
+    print('Abilities: ')
+    for ab in rspn['abilities']:
+        print(' - ' + ab['ability']['name'])
+else:
+    print("You know nothing about Pokemon!! What a shame!!!")
